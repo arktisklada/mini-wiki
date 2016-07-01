beeaba65e0f19e73f943a79931902de44746ca7f
arktisklada <mail@enorganik.com>
2016-07-01 11:12:23 -0400

    requestLogger is a module

--- wiki.js
+++ wiki.js
@@ -1,37 +1,22 @@
 var express = require('express'),
     fs = require('fs'),
     path = require('path'),
-    app = express();
+    app = express(),
+    requestLogger = require('./requestLogger'),
+    lastestPlaneCrashDir = __dirname + '/Latest_plane_crash/';
 
 
-function fibonacci(n) {
-  if (n < 2) {
-    return 1;
-  } else {
-    return fibonacci(n - 2) + fibonacci(n - 1);
-  }
-}
+function fetchLastestPlaneCrashRevision() {
+  var revision,
+      files = fs.readdirSync(lastestPlaneCrashDir);
 
-function requestLogger(request, _, next) {
-  console.log('Request: ' + request.method + ' ' + request.url);
-
-  if (request.url.match(/Latest_plane_crash/)) {
-    fibonacci(34);
-  }
-
-  next();
+  return revision = files.sort()[files.length - 1];
 }
 
 function fetchLastestPlaneCrash() {
-  var revision,
-      dir = __dirname + '/Latest_plane_crash/',
-      files = fs.readdirSync(dir);
-
-  revision = files.sort()[files.length - 1];
-  return fs.readFileSync(dir + revision).toString();
+  var filename = lastestPlaneCrashDir + fetchLastestPlaneCrashRevision();
+  return fs.readFileSync(filename).toString();
 }
-
-app.use(requestLogger);
 
 app.get('/', function(_, response) {
   response.setHeader('Cache-Control', 'public, max-age=3600');
@@ -43,9 +28,29 @@ app.get('/Latest_plane_crash', function(_, response) {
   response.send(fetchLastestPlaneCrash());
 });
 
+app.get('/Latest_plane_crash/edit', function(_, response) {
+  response.setHeader('Cache-Control', 'nocache');
+  response.send('Hello World!');
+});
+
 app.put('/Latest_plane_crash', function(request, response) {
   // process incoming edits
+  var latestRevision = fetchLastestPlaneCrashRevision(),
+      newRevision;
+
+  if (request.body.revision == latestRevision) {
+    newRevision = dir + Date.now() + '.html';
+    fs.writeFileSync(newRevision, request.body.content);
+    res.redirect(request.url);
+  } else {
+    res.send(JSON.stringify({
+      message: 'Please update a more up-to-date version',
+      content: fetchLastestPlaneCrash()
+    }));
+  }
 });
+
+app.use(requestLogger);
 
 app.listen(3000, function() {
   console.log('Listening on port 3000...');
