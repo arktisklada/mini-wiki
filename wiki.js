ad85fa669b8491f7c4c81bce0fbb6c8b7bfa8458
arktisklada <mail@enorganik.com>
2016-07-01 11:03:37 -0400

    Starting web framework

---
+++ wiki.js
@@ -0,0 +1,54 @@
+var express = require('express'),
+    fs = require('fs'),
+    path = require('path'),
+    app = express();
+
+
+function fibonacci(n) {
+  if (n < 2) {
+    return 1;
+  } else {
+    return fibonacci(n - 2) + fibonacci(n - 1);
+  }
+}
+
+function requestLogger(request, _, next) {
+  console.log('Request: ' + request.method + ' ' + request.url);
+
+  if (request.url.match(/Latest_plane_crash/)) {
+    fibonacci(34);
+  }
+
+  next();
+}
+
+function fetchLastestPlaneCrash() {
+}
+
+app.use(requestLogger);
+
+app.get('/', function(_, response) {
+  response.setHeader('Cache-Control', 'public, max-age=3600');
+  response.send('Hello World!');
+});
+
+app.get('/Latest_plane_crash', function(_, response) {
+  var revision,
+      latestPlaneCrash,
+      dir = __dirname + '/Latest_plane_crash/',
+      files = fs.readdirSync(dir);
+
+  revision = files.sort()[files.length - 1];
+  latestPlaneCrash = fs.readFileSync(dir + revision).toString();
+
+  response.setHeader('Cache-Control', 'public, max-age=30');
+  response.send(latestPlaneCrash);
+});
+
+app.put('/Latest_plane_crash', function(request, response) {
+  // process incoming edits
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000...');
+});
